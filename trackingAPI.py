1a579f6c495dc7f7d0a729c750c4ae66a5974b57
Trey Stegeman <trey363636@gmail.com>
2022-06-01 16:04:45 -0400

    Define get tracking number

--- trackingAPI.py
+++ trackingAPI.py
@@ -1,7 +1,4 @@
-import base64
-
 from requests.auth import HTTPBasicAuth
-
 import config
 import json
 import requests
@@ -14,18 +11,17 @@ def track(tracking_number):
     # FedEx Authorization API Documentation
     # https://developer.fedex.com/api/en-us/catalog/track/docs.html
     auth_url = fedex_url + "oauth/token"
-    auth_input = {
+    auth_payload = {
         'grant_type': 'client_credentials',
         'client_id': config.API_KEY,  # Public key
         'client_secret': config.API_SECRET  # Secret key
     }
-    auth_payload = auth_input
     auth_headers = {
         'Content-Type': "application/x-www-form-urlencoded"
     }
     auth_response = requests.request("POST", url=auth_url, data=auth_payload, headers=auth_headers)
-    response_dict = json.loads(auth_response.text)
-    access_token = response_dict['access_token']  # Get bearer token out of json response
+    response_dict = json.loads(auth_response.text)  # Turn json response into dictionary
+    access_token = response_dict['access_token']  # Get bearer token out of response dictionary
     # FedEx Track API Documentation
     # https://developer.fedex.com/api/en-us/catalog/track/docs.html
     track_url = fedex_url + "track/v1/trackingnumbers"
@@ -42,26 +38,39 @@ def track(tracking_number):
     track_headers = {
         'Content-Type': "application/json",
         'X-locale': "en_US",
-        'Authorization': "Bearer " + access_token  # Adds access token to header for authorization
+        'Authorization': "Bearer " + access_token  # Adds  bearer access token to header for authentication
     }
     track_response = requests.request("POST", url=track_url, data=track_input, headers=track_headers)
     tracking_dict = json.loads(track_response.text)  # Make dictionary out of json response
     print("FedEx " + track_headers["Authorization"])
-    print(json.dumps(tracking_dict["output"]["completeTrackResults"][0]["trackResults"][0]["scanEvents"], indent=4))
+    return json.dumps(
+        tracking_dict["output"]["completeTrackResults"][0]["trackResults"][0]["scanEvents"][0])  # Get scan events
 
 
-def getTickets():
+def getTicketsList():
     jitbit_url = "https://shsupport.jitbit.com/helpdesk/api/tickets?mode=handledbyme&count=300"
-    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)
+    # JitBit Helpdesk API Documentation
+    # https://www.jitbit.com/helpdesk/helpdesk-api/#/
+    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)  # Basic authorization headers
     jitbit_response = requests.get(url=jitbit_url, auth=jitbit_auth)
-    tickets = json.loads(jitbit_response.content)
+    tickets = json.loads(jitbit_response.content)  # All assigned tickets information
     print(jitbit_auth)
-    print(json.dumps(tickets, indent=4))
     assigned_tickets_list = []
-    for x in range(len(tickets)):
+    for x in range(len(tickets)):  # Go through list of tickets and make an array of ticket numbers
         assigned_tickets_list.append(json.dumps(tickets[x]["IssueID"]))
-    print(assigned_tickets_list)
+    return assigned_tickets_list
+
+
+def getTrackingNumber(ticket_number):
+    jitbit_url = "https://shsupport.jitbit.com/helpdesk/api/ticket?id=" + str(ticket_number)
+    # JitBit Helpdesk API Documentation
+    # https://www.jitbit.com/helpdesk/helpdesk-api/#/
+    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)  # Basic authorization headers
+    jitbit_response = requests.get(url=jitbit_url, auth=jitbit_auth)
+    ticket = json.loads(jitbit_response.content)  # Tickets detailed information
+    return str(json.dumps(ticket["Tags"][0]["Name"])[1:-1])
 
 
 track(tracking_number)
-getTickets()
+getTicketsList()
+getTrackingNumber(48524901)
