c60daf38dbee26d0618bba9c69daabe793be0fd8
Trey Stegeman <trey363636@gmail.com>
2022-06-02 14:22:38 -0400

    Define create tracking update

--- trackingAPI.py
+++ trackingAPI.py
@@ -2,8 +2,29 @@ from requests.auth import HTTPBasicAuth
 import config
 import json
 import requests
+from datetime import datetime
 
-tracking_number = "776958984516"
+
+def getTicketsList():
+    jitbit_url = "https://shsupport.jitbit.com/helpdesk/api/tickets?mode=handledbyme&count=300"
+    # JitBit Helpdesk API Documentation
+    # https://www.jitbit.com/helpdesk/helpdesk-api/#/
+    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)  # Basic authorization headers
+    jitbit_response = requests.get(url=jitbit_url, auth=jitbit_auth)
+    tickets = json.loads(jitbit_response.content)  # All assigned tickets information
+    print(jitbit_auth)
+    assigned_tickets_list = []
+    for x in range(len(tickets)):  # Go through list of tickets and make an array of ticket numbers
+        assigned_tickets_list.append(json.dumps(tickets[x]["IssueID"]))
+    return assigned_tickets_list  # Return list of assigned tickets
+
+
+def getTrackingNumber(ticket_number):
+    jitbit_url = "https://shsupport.jitbit.com/helpdesk/api/ticket?id=" + str(ticket_number)
+    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)  # Basic authorization headers
+    jitbit_response = requests.get(url=jitbit_url, auth=jitbit_auth)
+    ticket = json.loads(jitbit_response.content)  # Tickets detailed information
+    return str(json.dumps(ticket["Tags"][0]["Name"])[1:-1])  # Return ticket's tag
 
 
 def track(tracking_number):
@@ -43,34 +64,20 @@ def track(tracking_number):
     track_response = requests.request("POST", url=track_url, data=track_input, headers=track_headers)
     tracking_dict = json.loads(track_response.text)  # Make dictionary out of json response
     print("FedEx " + track_headers["Authorization"])
-    return json.dumps(
-        tracking_dict["output"]["completeTrackResults"][0]["trackResults"][0]["scanEvents"][0])  # Get scan events
+    return json.dumps(tracking_dict["output"]["completeTrackResults"][0]["trackResults"][0], indent=4)  # Get scan events
 
 
-def getTicketsList():
-    jitbit_url = "https://shsupport.jitbit.com/helpdesk/api/tickets?mode=handledbyme&count=300"
-    # JitBit Helpdesk API Documentation
-    # https://www.jitbit.com/helpdesk/helpdesk-api/#/
-    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)  # Basic authorization headers
-    jitbit_response = requests.get(url=jitbit_url, auth=jitbit_auth)
-    tickets = json.loads(jitbit_response.content)  # All assigned tickets information
-    print(jitbit_auth)
-    assigned_tickets_list = []
-    for x in range(len(tickets)):  # Go through list of tickets and make an array of ticket numbers
-        assigned_tickets_list.append(json.dumps(tickets[x]["IssueID"]))
-    return assigned_tickets_list
+def createTrackingUpdate(track_info):
+    scan_events = json.loads(track_info)
+    time = datetime.strptime(scan_events["scanEvents"][0]['date'].split('T')[1][0:5], "%H:%M")
+    print("AUTOMATED TRACKING UPDATE: " +
+          scan_events["scanEvents"][0]['eventDescription'] + " in " +
+          scan_events["scanEvents"][0]['scanLocation']['city'].title() + ", " +
+          scan_events["scanEvents"][0]['scanLocation']['stateOrProvinceCode'] + " at " +
+          time.strftime("%I:%M%p") + " " +
+          scan_events["scanEvents"][0]['date'].split('T')[0] + ".")
 
 
-def getTrackingNumber(ticket_number):
-    jitbit_url = "https://shsupport.jitbit.com/helpdesk/api/ticket?id=" + str(ticket_number)
-    # JitBit Helpdesk API Documentation
-    # https://www.jitbit.com/helpdesk/helpdesk-api/#/
-    jitbit_auth = HTTPBasicAuth(config.JitBit_Username, config.JitBit_Password)  # Basic authorization headers
-    jitbit_response = requests.get(url=jitbit_url, auth=jitbit_auth)
-    ticket = json.loads(jitbit_response.content)  # Tickets detailed information
-    return str(json.dumps(ticket["Tags"][0]["Name"])[1:-1])
-
-
-track(tracking_number)
 getTicketsList()
-getTrackingNumber(48524901)
+createTrackingUpdate(track(getTrackingNumber(48524901)))
+
